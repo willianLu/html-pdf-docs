ae4cd8618097a0e092070cc3fc5bbde0c4244a7b
kunkun.lu <1013658157@qq.com>
2025-03-26 15:02:59 +0800

    deploy: 202503261502

--- assets/HomeView-yxYZcci1.js
+++ assets/HomeView-yxYZcci1.js
@@ -1,4 +1,4 @@
-import{d as n,c as a,a as r,o,b as c,u as l,_ as i}from"./index-DPRjzxbp.js";const h=""+new URL("example-B_uq3Jhe.jpg",import.meta.url).href,m={class:"markdown-body"},p=n({__name:"README",setup(s,{expose:d}){return d({frontmatter:{},excerpt:void 0}),(t,e)=>(o(),a("div",m,e[0]||(e[0]=[r(`<h1>html-to-pdf 文档手册</h1><p>本库致力于 html 在线导出为 pdf，实现智能分页效果，不会切断文本图片表格等内容，可兼容手机端。本库依赖 html2canvas 和 jspdf，使用 canvas 绘图，请保证使用浏览器对以上库是支持的。</p><h3>安装</h3><pre><code class="language-bash"># 使用npm
+import{d as n,c as a,a as r,o,b as c,u as l,_ as i}from"./index-BEpdIeYo.js";const h=""+new URL("example-B_uq3Jhe.jpg",import.meta.url).href,m={class:"markdown-body"},p=n({__name:"README",setup(s,{expose:d}){return d({frontmatter:{},excerpt:void 0}),(t,e)=>(o(),a("div",m,e[0]||(e[0]=[r(`<h1>html-to-pdf 文档手册</h1><p>本库致力于 html 在线导出为 pdf，实现智能分页效果，不会切断文本图片表格等内容，可兼容手机端。本库依赖 html2canvas 和 jspdf，使用 canvas 绘图，请保证使用浏览器对以上库是支持的。</p><h3>安装</h3><pre><code class="language-bash"># 使用npm
 npm i -S ai-html-to-pdf
 # 使用yarn
 yarn add ai-html-to-pdf
